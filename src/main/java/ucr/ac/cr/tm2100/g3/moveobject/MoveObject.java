c3982e4cd169c2d7dac8d33a7981f00d6e125ff6
galagarza1986 <aaron.galagarzacarrillo@ucr.ac.cr>
2024-09-02 15:40:54 -0600

    primer commit proyecto moveObject

---
+++ src/main/java/ucr/ac/cr/tm2100/g3/moveobject/MoveObject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ucr.ac.cr.tm2100.g3.moveobject;
+
+/**
+ *
+ * @author Aarón Galagarza
+ */
+public class MoveObject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
